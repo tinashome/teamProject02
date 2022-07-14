2ae448cfaa651d203b69bc61be5f8e5b3a016295
Changhyeon Bae <pek5302@naver.com>
2022-07-14 16:17:59 +0900

    [#7] Setting: 경기장 상세페이지 파일 구조 세팅

---
+++ client/src/components/organisms/GroundInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GroundDetailInfo = () => {
+  <>경기장 상세 정보</>;
+};
+
+export default GroundDetailInfo;
---
+++ client/src/pages/Ground.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Ground = () => (
+  <>Ground</>
+);
+
+export default Ground;
