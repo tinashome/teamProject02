5cc9becd77c2871e0a3d320d7acb8e9af8bb1bb6
Changhyeon Bae <pek5302@naver.com>
2022-07-14 16:17:59 +0900

    [#7] Setting: 경기장 상세페이지 파일 구조 세팅

---
+++ client/src/components/atoms/TimeButton.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const TimeButton = styled.button `
+  width: 210px;
+  height: 40px;
+  background: #BAC8FF;
+  border-radius: 5px;
+`;
+
+export default TimeButton
