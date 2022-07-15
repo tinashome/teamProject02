d6c67e1f411a483b271a4805b89c84502d24cfb5
ctd <qpflapffhs76@gmail.com>
2022-07-16 02:49:43 +0900

    [#12] Feat: 필터링 및 검색창 레이아웃 (FE)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BannerImage from 'components/atoms/BannerImage';
 import GroundCard from 'components/organisms/GroundCard';
 import banner from 'assets/image/banner.jpeg';
@@ -8,50 +8,98 @@ import {
   FaAngleRight,
   FaAngleDoubleLeft,
   FaAngleDoubleRight,
+  FaAngleDown,
+  FaSearch,
 } from 'react-icons/fa';
 import groundListDummy from './groundListDummy';
 
-const Home = () => (
-  <>
-    <BannerImage src={banner} />
-    <Container>
-      {/* <FilterWrapper>
-        <button type='button'>모든 지역</button>
-        <button type='button'>완료 제외</button>
-        <input placeholder='구장 찾기' />
-      </FilterWrapper> */}
-      <GroundList>
-        {groundListDummy.map((ground) => (
-          <GroundCard ground={ground} />
-        ))}
-      </GroundList>
-      <PaginationWrapper>
-        <FaAngleDoubleLeft />
-        <FaAngleLeft />
-        <ButtonWrapper>
-          <PageButton>1</PageButton>
-          <PageButton>2</PageButton>
-          <PageButton>3</PageButton>
-          <PageButton>4</PageButton>
-          <PageButton>5</PageButton>
-        </ButtonWrapper>
-        <FaAngleRight />
-        <FaAngleDoubleRight />
-      </PaginationWrapper>
-    </Container>
-  </>
-);
+const Home = () => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChangeSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+  return (
+    <>
+      <BannerImage src={banner} />
+      <Container>
+        <FilterWrapper>
+          <AreaFilter>
+            <FilterName>모든 지역</FilterName>
+            <FaAngleDown />
+          </AreaFilter>
+          <SearchBar>
+            <FaSearch />
+            <StyledInput
+              placeholder='구장 찾기'
+              value={searchInput}
+              onChange={handleChangeSearchInput}
+            />
+          </SearchBar>
+        </FilterWrapper>
+        <GroundList>
+          {groundListDummy.map((ground) => (
+            <GroundCard ground={ground} />
+          ))}
+        </GroundList>
+        <PaginationWrapper>
+          <FaAngleDoubleLeft />
+          <FaAngleLeft />
+          <ButtonWrapper>
+            <PageButton>1</PageButton>
+            <PageButton>2</PageButton>
+            <PageButton>3</PageButton>
+            <PageButton>4</PageButton>
+            <PageButton>5</PageButton>
+          </ButtonWrapper>
+          <FaAngleRight />
+          <FaAngleDoubleRight />
+        </PaginationWrapper>
+      </Container>
+    </>
+  );
+};
 
 const Container = styled.div`
-  padding: 2rem 10rem;
+  padding: 1.5rem 10rem;
 `;
 
-// const FilterWrapper = styled.div`
-//   display: flex;
-//   justify-content: space-evenly;
-//   padding: 1rem 0;
-//   margin-bottom: 2rem;
-// `;
+const FilterWrapper = styled.div`
+  display: flex;
+  margin-bottom: 0.5rem;
+`;
+
+const AreaFilter = styled.button`
+  display: flex;
+  align-items: center;
+  margin: 0 2rem;
+  button {
+    margin-right: 0.5rem;
+  }
+`;
+
+const FilterName = styled.p`
+  margin-right: 0.5rem;
+`;
+
+const SearchBar = styled.div`
+  width: 18rem;
+  display: flex;
+  border: 1px solid #ced4da;
+  border-radius: 24px;
+  padding: 12px 16px;
+  margin-right: 8rem;
+  svg {
+    opacity: 0.5;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 90%;
+  border: none;
+  outline: none;
+  padding-left: 1rem;
+`;
 
 const GroundList = styled.section`
   display: flex;
