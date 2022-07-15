ef0daa4268cfcfd1758a94ae60ccb5dd34341e1e
이수정 <aqqlee@gamil.com>
2022-07-16 01:02:45 +0900

    [#9]	Feat:	/admin 라우터 추가

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
+import Admin from '../pages/Admin';
 import GlobalLayout from './GlobalLayout';
 
 const Router = () => (
@@ -12,6 +13,7 @@ const Router = () => (
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/admin' element={<Admin />} />
       </Route>
     </Routes>
   </BrowserRouter>
