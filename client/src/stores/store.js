e6111f15e7a36ca2a2fe3ca9850baf367fb87b9b
이수정 <aqqlee@gamil.com>
2022-07-17 06:20:55 +0900

    [#21] Refactor: store분리 (FE)

--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -5,15 +5,4 @@ const textState = atom({
   default: '', // default value (aka initial value)
 });
 
-// 관리페이지 메뉴선택하는 상태
-const adminContentState = atom({
-  key: 'sideMenu',
-  default: [],
-});
-
-// 관리페이지 유저목록 api요청결과값을 저장하는 상태
-const adminUsers = atom({
-  key: 'adminUsers',
-  default: [],
-});
-export { textState, adminContentState, adminUsers };
+export default textState;
