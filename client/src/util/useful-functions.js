96f3200bf3e5a3f3199dc829080c2119551be8a4
ctd <qpflapffhs76@gmail.com>
2022-07-18 20:39:37 +0900

    [#26] Feat: 경기장 정보 - 포인트 부분 숫자에 쉼표 추가 (FE)

--- client/src/util/useful-functions.js
+++ client/src/util/useful-functions.js
@@ -1,3 +1,4 @@
+// 토큰이 있는지 확인
 export const isExistToken = () => {
   if (localStorage.getItem('token')) {
     return true;
@@ -5,4 +6,13 @@ export const isExistToken = () => {
   return false;
 };
 
+// 토큰 가져오기
 export const getToken = () => localStorage.getItem('token');
+
+// 숫자에 쉼표를 추가함. (10000 -> 10,000)
+export const addCommas = (n) =>
+  n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+// 휴대폰 번호 하이픈 추가
+export const addHyphen = (n) =>
+  n.toString().replace(/\B(?=(\d{4})+(?!\d))/g, '-');
