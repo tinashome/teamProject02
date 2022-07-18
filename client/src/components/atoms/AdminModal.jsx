bce4130c258a808eabc591a7c5c47d3a81339bef
이수정 <aqqlee@gamil.com>
2022-07-18 12:54:21 +0900

    [#22] Refactor: admin modal atom분리 (FE)

---
+++ client/src/components/atoms/AdminModal.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ModalButton = styled.button`
+  width: 80px;
+  height: 50px;
+  padding: 5px 10px;
+  margin-top: 20px;
+  border-radius: 4px;
+  background: #3563e9;
+  color: white;
+  text-align: center;
+  font-size: 25px;
+`;
+
+export default ModalButton;
