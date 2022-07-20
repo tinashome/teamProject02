b3d11edd30ad3e371b1aebb0ece12b4f5ea14665
ctd <qpflapffhs76@gmail.com>
2022-07-20 14:53:44 +0900

    [#31] Fix: List Type Default 값 수정, Card 크기 수정 (FE)

--- client/src/components/organisms/GroundCard.jsx
+++ client/src/components/organisms/GroundCard.jsx
@@ -29,9 +29,9 @@ const GroundCard = ({ ground }) => {
 };
 
 const Container = styled.div`
-  /* width: 20%; */
+  display: flex;
+  flex-direction: column;
   min-width: fit-content;
-  height: 20rem;
   margin: 1rem;
   border: 1px solid #adb5bd;
   border-radius: 4px;
@@ -40,7 +40,7 @@ const Container = styled.div`
 
 const GroundImage = styled.img`
   width: 100%;
-  height: 50%;
+  height: 200px;
 `;
 
 const GroundAddress = styled.p`
@@ -57,6 +57,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  margin-bottom: 0.5rem;
 `;
 
 const PaymentPoint = styled.p`
