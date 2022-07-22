5e0630573fc52d3c80a0b8a0f43cf1d70ef434cc
Changhyeon Bae <pek5302@naver.com>
2022-07-23 02:45:27 +0900

    [#19] Feat: 경기장 운영시간 버튼 상태 변화 기능 구현

--- client/src/components/atoms/TimeButton.jsx
+++ client/src/components/atoms/TimeButton.jsx
@@ -1,11 +1,36 @@
+import React from 'react';
 import styled from 'styled-components';
 
 const TimeButton = styled.button`
   width: 210px;
   height: 40px;
+  font-size: 15px;
+  font-weight: bold;
   background: #bac8ff;
-  border-radius: 5px;
+  border: solid #bdbdbd;
+  border-radius: 3px;
   margin: 3px;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
 
+const TimeBtn = ({ renderTime, startTime, endTime }) => {
+  // renderTime, startTime, endTime Slice 작업 필요 type도 number로 변경
+  const startTimeSlice = Number(renderTime?.slice(0, 2));
+  const endTimeSlice = Number(renderTime?.slice(6, 8));
+  const startTimeFormat = Number(startTime?.slice(0, 2));
+  const endTimeFormat = Number(endTime?.slice(0, 2));
+  // reservationTime.includes(renderTime) => 날짜마다 예약 일자 날짜도 넣기
+
+  const isAble =
+    startTimeSlice < startTimeFormat || endTimeSlice > endTimeFormat;
+  return (
+    <TimeButton value={renderTime} disabled={isAble}>
+      {renderTime}
+    </TimeButton>
+  );
+};
+
 export default TimeButton;
+export { TimeBtn };
--- client/src/components/organisms/GroundTime.jsx
+++ client/src/components/organisms/GroundTime.jsx
@@ -3,51 +3,12 @@ import styled from 'styled-components';
 import { BiTimeFive } from 'react-icons/bi';
 import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/bs';
 import { useRecoilState } from 'recoil';
-import timeValue from 'stores/reservationStore';
-import TimeButton from '../atoms/TimeButton';
+import { morningTimeValue, afternoonTimeValue } from 'constants/TimeBtnValue';
+import TimeButton, { TimeBtn } from '../atoms/TimeButton';
 
 const GroundTime = ({ info }) => {
   const [timeBtnShow, setTimeBtnShow] = useState(true);
-  const [timeBtnValue, setTimeBtnValue] = useRecoilState(timeValue);
-  const [startTimeValue, setStartTimeValue] = useState(info.startTime);
-  const [endTimeValue, setEndTimeValue] = useState(info.endTime);
-
-  const timeBtnState = () => {
-    const newTimeList = [...timeBtnValue];
-    const startTimeSlice = newTimeList.map((list) => list.value.slice(0, 5)); // 11:00
-    const endTimeSlice = newTimeList.map((list) => list.value.slice(6, 11)); // 21:00 ~ 22:00
-    const startTimeIndex = startTimeSlice.indexOf(startTimeValue);
-    const endTimeIndex = endTimeSlice.indexOf(endTimeValue);
-
-    // 시작 시간 이전 버튼 비활성화
-    for (let i = 0; i < startTimeIndex; i += 1) {
-      newTimeList[i] = { ...newTimeList[i], reservation: true };
-    }
-
-    // 마감 시간 이후 버튼 비활성화
-    // for (let i = endTimeIndex + 1; i < newTimeList.length; i += 1) {
-    //   newTimeList[i] = { ...newTimeList[i], reservation: true };
-    // }
-    // console.log(newTimeList)
-    setTimeBtnValue(newTimeList);
-  };
-
-  useEffect(() => {
-    const startTimeFormat = `${startTimeValue?.slice(
-      0,
-      2,
-    )}:${startTimeValue?.slice(2, 4)}`; // 0700 => 07:00
-    const endTimeFormat = `${endTimeValue?.slice(0, 2)}:${endTimeValue?.slice(
-      2,
-      4,
-    )}`; // 2300 => 23:00
-    setStartTimeValue(startTimeFormat);
-    setEndTimeValue(endTimeFormat);
-    timeBtnState();
-    console.log(info);
-
-  }, [startTimeValue, endTimeValue]);
-
+  const { startTime, endTime } = info;
   const handleClick = () => {
     setTimeBtnShow(!timeBtnShow);
   };
@@ -67,14 +28,17 @@ const GroundTime = ({ info }) => {
         </ShowBtn>
       </DateTimeNavbar>
       <Container style={timeBtnShow ? { display: '' } : { display: 'none' }}>
-        <Title>오전 / 오후</Title>
+        <Title>오전</Title>
         <TimeBtns>
-          {/* */}
-          {timeBtnValue.map((list) => (
-            <Button key={list.id} disabled={list.reservation}>
-              {list.value} 
-            </Button>
-          ))}
+          {morningTimeValue.map((renderTime) =>
+            TimeBtn({ renderTime, startTime, endTime }),
+          )}
+        </TimeBtns>
+        <Title>오후</Title>
+        <TimeBtns>
+          {afternoonTimeValue.map((renderTime) =>
+            TimeBtn({ renderTime, startTime, endTime }),
+          )}
         </TimeBtns>
       </Container>
     </>
@@ -99,16 +63,11 @@ const Title = styled.h1`
 const TimeBtns = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  width: 100%;
+  width: 75rem;
   margin: 1rem 0 1rem 1rem;
   gap: 0.5rem 0;
-`;
-
-const Button = styled(TimeButton)`
-  &:hover {
-    color: white;
-    background-color: #3563e9;
-  }
+  border: solid #bdbdbd;
+  padding:1rem;
 `;
 
 const DateTimeNavbar = styled.div`
