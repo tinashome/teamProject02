5f33380c9cbf145da76bb4b60a10f198dd5c0785
ctd <qpflapffhs76@gmail.com>
2022-07-27 17:25:33 +0900

    [#62] Design: 공통 모달 CSS 애니메이션 추가 (FE)

--- client/src/components/atoms/AdminModalDiv.jsx
+++ client/src/components/atoms/AdminModalDiv.jsx
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(400px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
 
 const ModalDiv = styled.div`
-  display: ${(props) => (props.modal === '' ? 'none' : 'flex')}};
-  position:absolute;
+  display: ${(props) => (props.modal === '' ? 'none' : 'flex')};
+  position: absolute;
   top: 50%;
   left: 50%;
   width: 300px;
@@ -11,12 +20,17 @@ const ModalDiv = styled.div`
   margin-top: -100px;
   padding: 20px;
   border-radius: 5px;
-  background-color:#fff;
+  background-color: #fff;
   justify-content: center;
   text-align: center;
   align-items: center;
   white-space: pre-line;
   flex-direction: column;
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
 `;
 
 export default ModalDiv;
--- client/src/components/atoms/AdminModalWrapper.jsx
+++ client/src/components/atoms/AdminModalWrapper.jsx
@@ -1,17 +1,30 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  } 100% {
+    opacity: 1;
+  }
+`;
 
 const ModalWrapper = styled.div`
-  display: ${(props) => (props.modal === '' ? 'none' : 'flex')}};
+  display: ${(props) => (props.modal === '' ? 'none' : 'flex')};
   position: fixed;
   z-index: 1000;
-  top:0;
-  left:0;
+  top: 0;
+  left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0,0,0,0.4);
+  background-color: rgba(0, 0, 0, 0.4);
   font-size: 30px;
   font-weight: 400;
   letter-spacing: -2px;
-  `;
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+`;
 
 export default ModalWrapper;
